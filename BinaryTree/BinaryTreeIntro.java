8f1fdeeb85cfecde84441f9cfb8c3936d87a024b
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-10-23 20:00:12 +0530

    tree traversal

--- BinaryTree/BinaryTreeIntro.java
+++ BinaryTree/BinaryTreeIntro.java
@@ -15,11 +15,36 @@ public class BinaryTreeIntro {
     BinaryTreeIntro() {
         root = null;
     }
+    static void inOrder(Node root){
+        if(root == null) return;
+        inOrder(root.left);
+        System.out.print(root.key+ " ");
+        inOrder(root.right);
+    }
+    static void PreOrder(Node root){
+        if(root == null) return;
+        System.out.print(root.key+" ");
+        PreOrder(root.left);
+        PreOrder(root.right);
+    }
+    static void PostOrder(Node root){
+        if(root == null) return;
+        PostOrder(root.left);
+        PostOrder(root.right);
+        System.out.print(root.key+" ");
+    }
     public static void main(String[] args) {
         BinaryTreeIntro tree = new BinaryTreeIntro();
         tree.root = new Node(1);
         tree.root.left = new Node(2);
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(8);
+        inOrder(tree.root);
+        System.out.println();
+        PreOrder(tree.root);
+        System.out.println();
+        PostOrder(tree.root);
+
     }
 }
