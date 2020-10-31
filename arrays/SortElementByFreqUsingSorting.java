8d70e199ace8a9713340444df11280d2187246e3
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-10-31 13:07:37 +0530

    sort element by frequency sorting

--- arrays/SortElementByFreqUsingSorting.java
+++ arrays/SortElementByFreqUsingSorting.java
@@ -1,6 +1,7 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class SortElementByFreqUsingSorting {
@@ -27,6 +28,27 @@ public class SortElementByFreqUsingSorting {
             quickSort(arr,pi+1, high);
         }
     }
+    // Function to sort by column
+    public static void sortbyColumn(int arr[][], int col)
+    {
+        // Using built-in sort function Arrays.sort
+        Arrays.sort(arr, new Comparator<int[]>() {
+
+            @Override
+            // Compare values according to columns
+            public int compare(final int[] entry1,
+                               final int[] entry2) {
+
+                // To sort in descending order revert
+                // the '>' Operator
+                if (entry1[col] > entry2[col])
+                    return 1;
+                else
+                    return -1;
+            }
+        });  // End of function call sort().
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the array : ");
@@ -37,22 +59,31 @@ public class SortElementByFreqUsingSorting {
             arr[i] = sc.nextInt();
         }
         quickSort(arr,0,size-1);
-        int[][] arrayMain = new int[size][2];
-        for(int i = 0; i < size; i++){
-            arrayMain[i][0] = arr[i];
-            arrayMain[i][1] = 0;
-        }
-        for(int i = 0; i < size; i++){
-            System.out.println(arrayMain[i][0]+" , "+arrayMain[i][1]);
-        }
-        for(int i =1; i < size; i++){
-            if(arrayMain[i][0] == arrayMain[i-1][0]){
-                arrayMain[i-1][1]++;
+//        System.out.println(Arrays.toString(arr));
+        int kthElement = 1;
+        int same = 0;
+        for(int i = 1; i < size; i++){
+            if(arr[i] != arr[same]) {
+                same = i;
+                kthElement++;
             }
         }
-        for(int i = 0; i < size; i++){
-            System.out.println(arrayMain[i][0]+" , "+arrayMain[i][1]);
-        }
 
+        int[][] arrayMain = new int[kthElement][2];
+        arrayMain[0][0] = arr[0];
+        arrayMain[0][1] = 0;
+        int j = 0;
+        for(int i = 0; i < size; i++){
+            if(arrayMain[j][0] != arr[i]){
+                j++;
+                arrayMain[j][0] = arr[i];
+
+            }
+            arrayMain[j][1] += 1;
+        }
+        sortbyColumn(arrayMain,1);
+        for(int i = 0; i < kthElement; i++){
+            System.out.println(arrayMain[i][0]+" "+ arrayMain[i][1]);
+        }
     }
 }
