58e24e04fa5877a45f1c50feec75811128753767
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-11-17 09:01:41 +0530

    construct tree from given inOrder and PreOder Traversal

--- BinaryTree/TreeFromInOrderPreOrderTraversal.java
+++ BinaryTree/TreeFromInOrderPreOrderTraversal.java
@@ -1,29 +1,35 @@
 package BinaryTree;
 
 import java.util.Scanner;
-
+// preOrder = 1 2 4 8 3
+// inOrder = 4 2 8 1 3
 public class TreeFromInOrderPreOrderTraversal {
-    static Node buildTree(int[] inOrder, int[] preOrder, int preIndex,int startIndex, int endIndex ){
+    static int preIndex = 0;
+    static Node buildTree(int[] inOrder, int[] preOrder,int startIndex, int endIndex ){
         if(startIndex > endIndex)
             return null;
         if(preIndex >= preOrder.length){
             return null;
         }
-        System.out.println("startIndex "+startIndex);
-        System.out.println("End Index "+endIndex);
-        System.out.println("preOrder "+preOrder[preIndex]);
+//        System.out.println("preIndex " + preIndex);
+//        System.out.println("startIndex "+startIndex);
+//        System.out.println("End Index "+endIndex);
+//        System.out.println("preOrder "+preOrder[preIndex]);
         Node toAdd = new Node(preOrder[preIndex]);
+        preIndex++;
+        if(startIndex == endIndex){ ;
+            return toAdd;
+        }
         int inIndex = -1;
         for(int i = 0; i < inOrder.length; i++){
-            if(inOrder[i] == preOrder[preIndex]) {
-                System.out.println("found in inOder "+inOrder[i]+ " i = " +i);
+            if(inOrder[i] == toAdd.key) {
+//                System.out.println("found in inOder "+inOrder[i]+ " i = " +i);
                 inIndex = i;
                 break;
             }
         }
-        preIndex++;
-        toAdd.left = buildTree(inOrder, preOrder, preIndex, startIndex, inIndex-1);
-        toAdd.right = buildTree(inOrder, preOrder, preIndex, inIndex+1, endIndex);
+        toAdd.left = buildTree(inOrder, preOrder, startIndex, inIndex-1);
+        toAdd.right = buildTree(inOrder, preOrder, inIndex+1, endIndex);
         return toAdd;
     }
     public static void main(String[] args) {
@@ -41,11 +47,9 @@ public class TreeFromInOrderPreOrderTraversal {
         for(int i = 0; i < size; i++){
             inOrder[i] = sc.nextInt();
         }
-        System.out.println("build tree called");
-        Node root = buildTree(inOrder, preOrder, 0,0,size-1);
-        System.out.println("PostOder Traversal of the Created Tree : ");
+        System.out.println("Build tree called");
+        Node root = buildTree(inOrder, preOrder,0,size-1);
+        System.out.println("InOder Traversal of the Created Tree : ");
         tree.inOrder(root);
-
-
     }
 }
