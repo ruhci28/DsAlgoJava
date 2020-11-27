927ab3e5aa9322cec2669da1335d62f21cf06cd6
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-11-27 09:21:06 +0530

    search and insert in BST

---
+++ BinarySearchTree/BST.java
@@ -0,0 +1,56 @@
+package BinarySearchTree;
+
+import BinaryTree.Node;
+
+public class BST {
+    static Node root;
+    BST(){
+        root = null;
+    }
+    static void insert(int key){
+        root = insertNode(root,key);
+    }
+    static Node insertNode(Node root,int key){
+        if(root == null){
+            root = new Node(key);
+            return root;
+        }
+        if(key < root.key){
+            root.left = insertNode(root.left,key);
+        }
+        else{
+            root.right = insertNode(root.right,key);
+        }
+        return root;
+    }
+
+    static void inOrder(Node root){
+        if(root == null) return;
+        inOrder(root.left);
+        System.out.print(root.key+ " ");
+        inOrder(root.right);
+    }
+    static Node search(Node root,int key){
+        if(root == null || root.key == key){
+            return root;
+        }
+        if(key < root.key){
+           return search(root.left,key);
+        }else{
+            return search(root.right,key);
+        }
+    }
+    public static void main(String[] args){
+        BST tree = new BST();
+        insert(6);
+        insert(3);
+        insert(8);
+        insert(1);
+        insert(4);
+        inOrder(tree.root);
+        Node searchedRoot = search(tree.root,4);
+        System.out.println();
+        System.out.println(searchedRoot.key);
+
+    }
+}
--- BinaryTree/Node.java
+++ BinaryTree/Node.java
@@ -1,10 +1,14 @@
 package BinaryTree;
 
 public class Node{
-    int key;
-    Node left, right;
+    public int key;
+    public Node left, right;
     public Node(int key){
         this.key = key;
+        right = left = null;
+    }
+    public Node(){
+        this.key = 0;
         right = left = null;
     }
 }
