d711fca58ee2ea7a94a24017faa80bbc49cf38c6
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-11-27 11:33:29 +0530

    InOrder Predeccor and Successor of a given key in BST

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -2,6 +2,8 @@ package BinarySearchTree;
 
 import BinaryTree.Node;
 
+import java.util.Random;
+
 public class BST {
     static Node root;
     BST(){
@@ -46,6 +48,60 @@ public class BST {
         }
         return root.key;
     }
+    static void delete(Node key){
+
+    }
+    static int maxValue(Node root){
+        if(root.right != null){
+            return maxValue(root.right);
+        }
+        return root.key;
+    }
+    static int getSuccessor(Node root, int data){
+        Node current = search(root,data);
+        if(current == null){
+            return -1;
+        }
+        if(current.right != null){
+            return minValue(current.right);
+        }
+        else {
+            Node successor = null;
+            Node ancestor = root;
+            while(ancestor != current){
+                if(ancestor.key>current.key){
+                    successor = ancestor;
+                    ancestor = ancestor.left;
+                }else {
+                    ancestor = ancestor.right;
+                }
+            }
+            return (successor != null) ? successor.key : -1;
+        }
+    }
+    static int getPredecessor(Node root, int data){
+        Node current = search(root,data);
+        if(current == null){
+            return -1;
+        }
+        if(current.left != null){
+            return maxValue(current.left);
+        }
+        else{
+            Node predecessor = null;
+            Node ancestor = root;
+            while (ancestor != current){
+                if(ancestor.key < current.key){
+                    predecessor = ancestor;
+                    ancestor = ancestor.right;
+                } else{
+                    ancestor = ancestor.left;
+                }
+            }
+            return (predecessor != null) ? predecessor.key : -1;
+        }
+
+    }
     public static void main(String[] args){
         BST tree = new BST();
         insert(6);
@@ -58,6 +114,8 @@ public class BST {
         System.out.println();
         System.out.println(searchedRoot.key);
         System.out.println(minValue(tree.root));
+        System.out.println("InOrder Successor of 4 is "+getSuccessor(tree.root,4));
+        System.out.println("Inorder Predecessor of 4 is "+getPredecessor(tree.root,4));
 
     }
 }
